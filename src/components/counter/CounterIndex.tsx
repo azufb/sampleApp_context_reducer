1c82d5ab60af3397a512010c422612a65829980b
azufb <curiousazunas@gmail.com>
2023-02-23 20:56:18 +0900

    ProviderとuseReducerを用いて、Contextで状態を管理できるようにした

---
+++ src/components/counter/CounterIndex.tsx
@@ -0,0 +1,20 @@
+import { useReducer } from 'react';
+import reducer from './reducer/reducer';
+import { CounterContext, CounterDispatchContext } from '../../contexts/context';
+import Counter from './Counter';
+import CurrentCounter from './CurrentCounter';
+
+const CounterIndex = () => {
+    // 初期値は0。
+    const [counter, dispatch] = useReducer(reducer, 0);
+    return (
+        <CounterContext.Provider value={counter}>
+            <CounterDispatchContext.Provider value={dispatch}>
+                <Counter />
+                <CurrentCounter />
+            </CounterDispatchContext.Provider>
+        </CounterContext.Provider>
+    );
+};
+
+export default CounterIndex;
