868f3f40a28d3fd4ba1c8e2aa68ff89f7918e90c
azufb <curiousazunas@gmail.com>
2023-02-23 22:39:15 +0900

    ルーティング修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
-import Form from './components/form/Form';
+import ListIndex from './components/form/ListIndex';
 import CounterIndex from './components/counter/CounterIndex';
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
-          <Route path='/form' element={<Form />} />
+          <Route path='/form' element={<ListIndex />} />
           <Route path='/counter' element={<CounterIndex />} />
         </Route>
       </Routes>
