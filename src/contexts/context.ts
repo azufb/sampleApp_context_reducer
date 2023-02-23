bd0c4da6939a55be01f8af014fdaa08310ac1588
azufb <curiousazunas@gmail.com>
2023-02-23 22:49:38 +0900

    ListのContext作成

--- src/contexts/context.ts
+++ src/contexts/context.ts
@@ -1,10 +1,16 @@
 import React, { createContext } from 'react';
 import { ActionType } from '../types/ActionType';
 
-export const CounterContext: React.Context<number> = createContext(0);
-
 const initialAction: ActionType = {
     type: ''
 };
+
+// List用のContext
+// 急遽anyで逃げる
+export const ListContext: any = createContext([]);
+export const ListDispatchContext: any = createContext(initialAction);
+
+// Counter用のContext
+export const CounterContext: React.Context<number> = createContext(0);
 // 急遽anyで逃げる
 export const CounterDispatchContext: any = createContext(initialAction);
