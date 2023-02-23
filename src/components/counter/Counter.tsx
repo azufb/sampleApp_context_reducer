db2bf534d73fe1514f93e95a78f1e41cd15ace38
azufb <curiousazunas@gmail.com>
2023-02-23 20:56:47 +0900

    加算減算処理の作成と現在のcounterの値表示

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,11 +1,48 @@
-const Counter = () => {
+import { useContext } from 'react';
+import { CounterContext, CounterDispatchContext } from '../../contexts/context';
+
+/**
+ * Counterコンポーネント
+ * 加算/減算処理を行う。
+ * 
+ * @returns {JSX.Element}
+ */
+const Counter = (): JSX.Element => {
+    // useContextで、stateを取得。
+    const currentCount: number = useContext(CounterContext);
+    // 一旦anyで逃げた。
+    const dispatch: any = useContext(CounterDispatchContext);
+
+    /**
+     * 加算
+     * 
+     * @returns {void}
+     */
+    const handleCounterAdd = (): void => {
+        dispatch({
+            type: 'add'
+        });
+    };
+
+    /**
+     * 減算
+     * 
+     * @returns {void}
+     */
+    const handleCounterSub = (): void => {
+        dispatch({
+            type: 'subtract'
+        });
+    };
+
     return (
         <div>
             <h2>Counterコンポーネント</h2>
 
             <div>
-                <button>+（加算）</button>
-                <button>-（減算）</button>
+                <button onClick={handleCounterAdd}>+</button>
+                <span>{currentCount}</span>
+                <button onClick={handleCounterSub}>-</button>
             </div>
         </div>
     );
