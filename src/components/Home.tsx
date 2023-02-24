b44de885abcfeba9ca665f9fa103314858ad0a80
azufb <curiousazunas@gmail.com>
2023-02-25 01:11:24 +0900

    Homeコンポーネントにスタイル適用と表示内容追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,22 @@
-const Home = () => {
+import { useContext } from 'react';
+import { UserInfoContext } from '../contexts/context';
+import { UserInfoType } from '../types/UserInfoType';
+import styles from '../styles/home.module.scss';
+
+/**
+ * Homeコンポーネント
+ *
+ * @returns {JSX.Element}
+ */
+const Home = (): JSX.Element => {
+  const userInfo: UserInfoType = useContext(UserInfoContext);
+
   return (
     <div>
-      <h2>Homeコンポーネント</h2>
+      <h2 className={styles.title}>Homeコンポーネント</h2>
+
+      <p>ユーザー名：{userInfo.userName}</p>
+      <p>ユーザーID：{userInfo.userId}</p>
     </div>
   );
 };
